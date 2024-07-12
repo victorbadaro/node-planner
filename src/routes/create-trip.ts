fa6e9535c4dcc94e7f9000141bc3ee6b2d6c4b40
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-12 15:49:27 -0300

    feat: add rule to create participants while creating a new trip

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -14,11 +14,12 @@ export async function createTrip(app: FastifyInstance) {
         starts_at: z.coerce.date(),
         ends_at: z.coerce.date(),
         owner_name: z.string(),
-        owner_email: z.string().email()
+        owner_email: z.string().email(),
+        emails_to_invite: z.array(z.string().email())
       })
     }
   }, async (request) => {
-    const { destination, starts_at, ends_at, owner_name, owner_email } = request.body;
+    const { destination, starts_at, ends_at, owner_name, owner_email, emails_to_invite } = request.body;
 
     if(dayjs(starts_at).isBefore(new Date())) {
       throw new Error('Invalid trip start date.');
@@ -32,7 +33,22 @@ export async function createTrip(app: FastifyInstance) {
       data: {
         destination,
         starts_at,
-        ends_at
+        ends_at,
+        participants: {
+          createMany: {
+            data: [
+              {
+                name: owner_name,
+                email: owner_email,
+                is_owner: true,
+                is_confirmed: true
+              },
+              ...emails_to_invite.map(email => {
+                return { email }
+              })
+            ]
+          }
+        }
       }
     });
 
