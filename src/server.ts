ffa61ccd0cac9bffbc0c4720ab959ae643c2f30a
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-12 19:52:07 -0300

    feat: add routes to create activities and links and routes to list them both

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmParticipant } from "./routes/confirm-participant";
 import { confirmTrip } from "./routes/confirm-trip";
+import { createActivity } from "./routes/create-activity";
+import { createLink } from "./routes/create-link";
 import { createTrip } from "./routes/create-trip";
+import { getActivities } from "./routes/get-activities";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -17,5 +21,9 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createTrip);
 app.register(confirmTrip);
 app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => console.log('Server is running at port 3333!'));
