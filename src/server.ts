9842dd95349bd3b498b14ed52f64957f3a7bbefd
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-12 16:21:48 -0300

    feat: add a basic route to confirm a trip

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
 import { createTrip } from "./routes/create-trip";
 
 const app = fastify();
@@ -8,5 +9,6 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(confirmTrip);
 
 app.listen({ port: 3333 }).then(() => console.log('Server is running at port 3333!'));
