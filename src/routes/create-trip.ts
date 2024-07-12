4fd0542e9d3aed801767d7fc165a34c1d63ad489
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-12 18:41:35 -0300

    refactor: separate dayjs logic into a different file

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,9 +1,9 @@
-import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import nodemailer from "nodemailer";
 import z from "zod";
+import { dayjs } from "../lib/dayjs";
 import { getMailClient } from "../lib/mail";
 import { prisma } from "../lib/prisma";
 
