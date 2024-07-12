e29bd2bdd9aa2a22694ec9ae378c05d420dad822
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-12 16:23:53 -0300

    feat: add cors rules

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import cors from "@fastify/cors";
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { confirmTrip } from "./routes/confirm-trip";
 import { createTrip } from "./routes/create-trip";
 
 const app = fastify();
+
+app.register(cors, {
+  origin: '*'
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
