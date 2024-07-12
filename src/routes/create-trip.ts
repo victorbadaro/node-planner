69362dbdc9146d683206ed8cb5071d1c38041ab6
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-12 15:13:22 -0300

    feat: add validation for starts_at and ends_at fields while creating a trip

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import z from "zod";
@@ -14,6 +15,15 @@ export async function createTrip(app: FastifyInstance) {
     }
   }, async (request) => {
     const { destination, starts_at, ends_at } = request.body;
+
+    if(dayjs(starts_at).isBefore(new Date())) {
+      throw new Error('Invalid trip start date.');
+    }
+
+    if(dayjs(ends_at).isBefore(starts_at)) {
+      throw new Error('Invalid trip end date.');
+    }
+
     const trip = await prisma.trip.create({
       data: {
         destination,
