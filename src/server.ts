996d1cc3b1bf437cdd3d590d510d3e9ba77a5741
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-07-13 00:50:22 -0300

    feat: add route to update a trip

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,7 @@ import { createTrip } from "./routes/create-trip";
 import { getActivities } from "./routes/get-activities";
 import { getLinks } from "./routes/get-links";
 import { getParticipants } from "./routes/get-participants";
+import { updateTrip } from "./routes/update-trip";
 
 const app = fastify();
 
@@ -29,5 +30,6 @@ app.register(createLink);
 app.register(getLinks);
 app.register(getParticipants);
 app.register(createInvite);
+app.register(updateTrip);
 
 app.listen({ port: 3333 }).then(() => console.log('Server is running at port 3333!'));
